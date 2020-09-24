dc8f79be5ec173e79f9fa973680a646b7fbe5822
lamquoctung <bighero6ts@gmail.com>
2020-09-24 14:58:56 +0700

    Tạo pull request

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SupplierTransactionComponent } from './supplier-transaction/supplier-transaction.component';
+import { CreateComponent } from './supplier-transaction/create/create.component';
+import { EditComponent } from './supplier-transaction/edit/edit.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [SupplierTransactionComponent, CreateComponent, EditComponent],
   imports: [
     CommonModule
   ]
---
+++ src/app/admin/supplier-transaction/supplier-transaction.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-supplier-transaction',
+  templateUrl: './supplier-transaction.component.html',
+  styleUrls: ['./supplier-transaction.component.css']
+})
+export class SupplierTransactionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
