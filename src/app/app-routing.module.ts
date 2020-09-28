191fc7cd595d8ebdf3df7f740e09eecff4f730b9
vipcafe <vipcafe1994@gmail.com>
2020-09-28 10:34:27 +0700

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {DailyScheduleComponent} from './home/daily-schedule/daily-schedule.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DailyScheduleComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/shared/models/DailySchedule.ts
@@ -0,0 +1,10 @@
+export interface DailySchedule {
+  id: number;
+  startTime: string;
+  endTime: string;
+  address: string;
+  note: string;
+  vaccinationDate: Date;
+  // vaccine: Vaccine;
+}
+
